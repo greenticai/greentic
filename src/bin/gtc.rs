d12e97f19c70e175c834047ea3d9592903d023e8
Vahe Grishkyan <vahe.grishkyan@ocado.com>
2026-03-28 00:08:04 +0400

    Update default operator image digest

--- src/bin/gtc.rs
+++ src/bin/gtc.rs
@@ -39,7 +39,7 @@ const BUNDLE_BIN: &str = "greentic-bundle";
 const DEPLOYER_BIN: &str = "greentic-deployer";
 const SETUP_BIN: &str = "greentic-setup";
 const DEFAULT_OPERATOR_IMAGE_DIGEST: &str =
-    "sha256:fa1c82477a03dc2642fdadf5f8d5dc818cb7ed99905b50e53fbab7fec763a8eb";
+    "sha256:caf0176a7f8d92a556b670c83fea235af213535293d5e024eae299635126d4b5";
 const EMBEDDED_TERRAFORM_GTPACK: &[u8] = include_bytes!(concat!(
     env!("CARGO_MANIFEST_DIR"),
     "/assets/deployer/terraform.gtpack"
